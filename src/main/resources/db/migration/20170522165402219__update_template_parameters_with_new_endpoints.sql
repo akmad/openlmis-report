72798604c1a38332af455cdf84b497ab307d5a37
Łukasz Lewczyński <llewczynski@soldevelo.com>
2017-07-19 10:46:05 +0200

    Removed malawi reports

--- src/main/resources/db/migration/20170522165402219__update_template_parameters_with_new_endpoints.sql
+++
@@ -1,2 +0,0 @@
-UPDATE template_parameters SET selectExpression = REPLACE(selectExpression, 'geographicZones/search?levelNumber=3', 'districts');
-UPDATE template_parameters SET selectExpression = REPLACE(selectExpression, '/api/', '/api/reports/');
